db1c4c147e5b7ee14e1080cda4d99119ce1f79f3
Beloved1310 <adeoluwafisayomi@gmail.com>
2024-01-29 11:47:42 +0100

    feat/project-set-up-client-home-and-chat-routes

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import React, { useState } from 'react'
+import { UserContext } from './UserContext'
+import Home from './components/Home/Home'
+import Chat from './components/Chat/Chat'
+
+function App() {
+  const [user, setUser] = useState('')
+  return (
+    <Router>
+      <div className="App">
+        <UserContext.Provider value={{ user, setUser }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/chat" element={<Chat />} />
+          </Routes>
+        </UserContext.Provider>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { UserContext } from '../../UserContext'
+
+
+const Chat = () => {
+  const {user, setUser} = useContext(UserContext)
+  return (
+    <div>
+      <h1> Chat {JSON.stringify(user)}</h1>
+      <Link to={'/home'}> <button>go to home</button></Link>
+    </div>
+  )
+}
+
+export default Chat
