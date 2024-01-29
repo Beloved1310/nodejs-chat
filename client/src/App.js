079923b9ddd97cdac8cac82f24646cd2164fedad
Beloved1310 <adeoluwafisayomi@gmail.com>
2024-01-29 12:14:41 +0100

    feat/add-nav-bar

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 import { UserContext } from './UserContext'
 import Home from './components/Home/Home'
 import Chat from './components/Chat/Chat'
+import Navbar from './components/layout/Navbar'
 
 function App() {
   const [user, setUser] = useState('')
@@ -11,6 +12,7 @@ function App() {
     <Router>
       <div className="App">
         <UserContext.Provider value={{ user, setUser }}>
+          <Navbar/>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/chat" element={<Chat />} />
